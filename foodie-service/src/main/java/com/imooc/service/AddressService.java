93b248d8aa4bf71f5bf1a2bb5deacf404d62e735
xiaodouzi <1950746552@qq.com>
2020-07-12 21:01:24 +0800

    1. 新增用户地址相关的操作 2. 用户地址的CRUD操作 5. 用户设置默认地址

---
+++ foodie-service/src/main/java/com/imooc/service/AddressService.java
@@ -0,0 +1,57 @@
+package com.imooc.service;
+
+import com.imooc.bo.AddressBO;
+import com.imooc.pojo.UserAddress;
+
+import java.util.List;
+
+/**
+ * 用户收货地址
+ *
+ * @author wangyong
+ */
+public interface AddressService {
+
+    /**
+     * 根据用户id查询用户的收获地址列表
+     *
+     * @param userId 用户id
+     * @return 用户收货地址
+     * @author wangyong
+     */
+    List<UserAddress> queryAll(String userId);
+
+    /**
+     * 用户新增地址
+     *
+     * @param addressBO
+     * @author wangyong
+     */
+    void addNewUserAddress(AddressBO addressBO);
+
+    /**
+     * 更新用户地址
+     *
+     * @param addressBO
+     * @author wangyong
+     */
+    void updateUserAddress(AddressBO addressBO);
+
+    /**
+     * 根据用户id和地址id删除收货地址
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     */
+    void deleteUserAddress(String userId, String addressId);
+
+    /**
+     * 修改默认地址
+     *
+     * @param userId    用户id
+     * @param addressId 地址id
+     * @author wangyong
+     */
+    void updateUserAddressToBeDefault(String userId, String addressId);
+
+}
---
+++ foodie-service/src/main/java/com/imooc/service/impl/AddressServiceImpl.java
@@ -0,0 +1,98 @@
+package com.imooc.service.impl;
+
+import com.imooc.bo.AddressBO;
+import com.imooc.enums.YesOrNo;
+import com.imooc.mapper.UserAddressMapper;
+import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.UserAddressExample;
+import com.imooc.service.AddressService;
+import org.n3r.idworker.Sid;
+import org.omg.CORBA.UserException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Resource
+    UserAddressMapper userAddressMapper;
+
+    @Autowired
+    Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<UserAddress> queryAll(String userId) {
+        UserAddressExample example = new UserAddressExample();
+        UserAddressExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(userId);
+
+        return userAddressMapper.selectByExample(example);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void addNewUserAddress(AddressBO addressBO) {
+        // 1. 判断当前用户是否存在地址
+        Integer isDefault = 0;
+        List<UserAddress> addressList = this.queryAll(addressBO.getUserId());
+        if (CollectionUtils.isEmpty(addressList)) {
+            isDefault = 1;
+        }
+        // 2. 保存地址到数据库
+        String addressId = sid.nextShort();
+        UserAddress newAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, newAddress);
+        newAddress.setId(addressId);
+        newAddress.setIsDefault(isDefault);
+        newAddress.setCreatedTime(new Date());
+        newAddress.setUpdatedTime(new Date());
+        userAddressMapper.insert(newAddress);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateUserAddress(AddressBO addressBO) {
+        String addressId = addressBO.getAddressId();
+        UserAddress userAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, userAddress);
+
+        userAddress.setId(addressId);
+        userAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.updateByPrimaryKeySelective(userAddress);
+
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void deleteUserAddress(String userId, String addressId) {
+        UserAddressExample example = new UserAddressExample();
+        UserAddressExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(userId);
+        criteria.andIdEqualTo(addressId);
+        userAddressMapper.deleteByExample(example);
+    }
+
+    @Override
+    public void updateUserAddressToBeDefault(String userId, String addressId) {
+        UserAddress userAddress = new UserAddress();
+        userAddress.setIsDefault(YesOrNo.NO.type);
+        UserAddressExample example = new UserAddressExample();
+        example.createCriteria().andUserIdEqualTo(userId);
+        userAddressMapper.updateByExampleSelective(userAddress, example);
+
+        userAddress.setIsDefault(YesOrNo.YES.type);
+        userAddress.setId(addressId);
+        userAddressMapper.updateByPrimaryKeySelective(userAddress);
+    }
+}
