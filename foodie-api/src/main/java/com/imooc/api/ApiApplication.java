d604d970c2dc7dd40624bbc3f99383e4f8a07d65
xiaodouzi <1950746552@qq.com>
2020-12-16 23:41:53 +0800

    1. SpringSession整合 2. 去除SpringSession安全自动装配

--- foodie-api/src/main/java/com/imooc/api/ApiApplication.java
+++ foodie-api/src/main/java/com/imooc/api/ApiApplication.java
@@ -3,18 +3,22 @@ package com.imooc.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @author wangyong
  * @time 2020/6/17 15:26
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @MapperScan(basePackages = "com.imooc.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.imooc", "org.n3r"})
 @EnableScheduling
+// 开启使用redis做为spring session
+@EnableRedisHttpSession
 public class ApiApplication {
 
     public static void main(String[] args) {
--- foodie-api/src/main/java/com/imooc/api/controller/HelloController.java
+++ foodie-api/src/main/java/com/imooc/api/controller/HelloController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping("/hello")
